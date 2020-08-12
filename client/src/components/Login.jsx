7ce4956bf0f209a363e1055ae22eac4c5ee68da4
galeazzij <galeazzij@users.noreply.github.com>
2020-08-12 16:11:58 -0400

    Revert "FE update Login component"
    This reverts commit c6ad78a7eaa6234a754994fa674de4213c27f88e.

--- client/src/components/Login.jsx
+++ client/src/components/Login.jsx
@@ -1,29 +1,10 @@
-import React, { useState } from 'react'
-import {Link} from 'react-router-dom'
+import React from 'react'
 
 export default function Login(props) {
-  const [formData, setFormData] = useState({
-    username: "",
-    password: ""
-  })
-
-  const handleChange = (e) => {
-    const { name, value } = e.target;
-    setFormData({
-      ...formData,
-      [name]: value
-    })
-  }
-  
+  debugger
   return (
-    <form>
-      <h3>Login</h3>
-      <input type="text" name="username" value={formData.username} onChange={handleChange} />
-      <input type="password" name="password" value={formData.password} onChange={handleChange} />
+    <div>
 
-      <Link>Register</Link>
-      <button>Submit</button>
-
-    </form>
+    </div>
   )
 }
