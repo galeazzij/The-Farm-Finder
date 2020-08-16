9f198d9209e958b34b3050c61a8d7fab5e38e851
galeazzij <galeazzij@users.noreply.github.com>
2020-08-16 11:36:51 -0400

    FE add functionality to cards along with styling for Home.jsx

--- client/src/components/NavBar.jsx
+++ client/src/components/NavBar.jsx
@@ -1,10 +1,5 @@
 import React from 'react'
-import {Navbar} from 'react-bootstrap'
-import {Nav} from 'react-bootstrap'
-import {NavDropdown} from 'react-bootstrap'
-import {Form} from 'react-bootstrap'
-import {FormControl} from 'react-bootstrap'
-import {Button} from 'react-bootstrap'
+import {Navbar, Nav, NavDropdown, Form, FormControl, Button} from 'react-bootstrap'
 
 
 
